3993cdfb19f16b826af5c4e39be78adbe8ded9cd
max <max.smith@duke.edu>
2020-03-18 15:03:58 -0400

    Created three types of functions in shell main.js

--- JavaScript/shell/js/main.js
+++ JavaScript/shell/js/main.js
@@ -1,5 +1,19 @@
-document.body.innerHTML="Conent is king";
-var a = 10;
-var b = 2;
-var prod = a*b;
-console.log("%i * %i = %i", a, b, prod);
+/* Named function */
+function named(name) {
+    var greeting = "Hi " + name + "! Welcome to my website.";
+    return greeting;
+}
+console.log(named("Max"));
+
+/* Anonymous function */
+var anonymous = function(name) {
+    var greeting = "Hi " + name + "! Welcome to my website.";
+    return greeting;
+}
+console.log(anonymous("Nick"));
+
+/* Immediately invokable expression */
+(function() {
+    var greeting = "Hi Noah! Welcome to my website.";
+    console.log(greeting);
+}())
