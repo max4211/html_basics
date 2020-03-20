5fc55e9503bae31f0b73947709b5459095cca5c8
max <max.smith@duke.edu>
2020-03-20 14:35:53 -0400

    completed javascript dynamic welcoming message

--- JavaScript/welcome/js/main.js
+++ JavaScript/welcome/js/main.js
@@ -1,19 +1,33 @@
-/* Named function */
-function named(name) {
-    var greeting = "Hi " + name + "! Welcome to my website.";
-    return greeting;
-}
-console.log(named("Max"));
+let getBody = document.querySelector("body");
+let newElement = document.createElement("h1");
 
-/* Anonymous function */
-var anonymous = function(name) {
-    var greeting = "Hi " + name + "! Welcome to my website.";
-    return greeting;
+/* Function to determine date */
+let welcomeMessage = function(hour) {
+    if (hour < 12) {
+        return "Good Morning!";
+    } else if (hour >= 12 && hour < 16) {
+        return "Good Afternoon!";
+    } else {
+        return "Good Evening!";
+    }
 }
-console.log(anonymous("Nick"));
 
-/* Immediately invokable expression */
-(function() {
-    var greeting = "Hi Noah! Welcome to my website.";
-    console.log(greeting);
-}())
+/* Create a date object to determine time of day */
+let date = new Date();
+let hour = date.getHours();
+console.log("hour: %i", hour);
+let message = welcomeMessage(hour);
+console.log("messsage: %s", message);
+
+/* Append message to website */
+let newText = document.createTextNode(message);
+newElement.appendChild(newText);
+getBody.appendChild(newElement);
+
+/* Add styling to the element */
+newElement.setAttribute("class", "welcome");
+newElement.style.color = "red";
+newElement.style.fontSize = "24px";
+newElement.style.textTransform = "uppercase";
+newElement.style.textAlign = "center";
+
