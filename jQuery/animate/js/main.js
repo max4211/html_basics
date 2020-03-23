20ae66b4800bf7ae739e545e5755f023a97719fe
max <max.smith@duke.edu>
2020-03-22 23:06:05 -0400

    Set up scroll html/css page

--- jQuery/animate/js/main.js
+++ jQuery/animate/js/main.js
@@ -3,12 +3,14 @@ $(document)
   .ready(function() {
     /* Selector then operate */
     $(".content-box").click(function() {
-        $(".content-box").animate({
+        $(".content-before").animate({
             width: '500px',
             height: '200px',
             padding: '100px 200px'
-        }, "slow");
-        $(".content-after").show().animate({opacity: '1'}, 3000);
+        }, "slow").animate({
+            fontSize: '32px'
+        }, 3000).fadeOut(2000)
+        /* $(".content-after").show().animate({opacity: '1'}, 3000); */
     });
 
   });
